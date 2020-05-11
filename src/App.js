6f197a566ebff8f04bf38354747d871d79e335df
Thai Nguyen <thainq00@gmail.com>
2020-05-11 16:02:33 +0700

    handle change background when clicking cell

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import {
-  useTable,
-  useGroupBy,
-  useFilters,
-  useSortBy,
-  useExpanded,
-  usePagination,
-} from "react-table";
+import Table from "./components/Table";
 
 const data = [
   { id: 1, name: "Wasif", age: 21, email: "wasif@email.com" },
@@ -20,19 +13,55 @@ const data = [
 const Array2D = (row, col, value) =>
   [...Array(row)].map((x) => Array(col).fill(value));
 
+const makeid = (length) => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 function App() {
   const [rowNum, setRowNum] = useState(20);
   const [colNum, setColNum] = useState(10);
   const [initArray, setInitArray] = useState([]);
+  let count = -1;
 
-  const handleClick = () => {
-    console.log(rowNum, colNum);
-    let arr = [...Array(rowNum)].map((x) => Array(colNum).fill(""));
+  const handleClickCreate = () => {
+    let arr = [...Array(rowNum)].map((x) => Array(colNum).fill(makeid(5)));
     arr.forEach((i) => {
       i[0] = arr.indexOf(i);
     });
-    console.log(arr);
+    for (let i = 0; i < arr[0].length; i++) {
+      arr[0][i] = ++count;
+    }
     setInitArray(arr);
+  };
+
+  const handleClickCell = (e) => {
+    let tungdo = e.target.parentNode.rowIndex;
+    let hoanhdo = e.target.cellIndex;
+
+    let th = e.target
+    th.parentNode.parentNode.childNodes.forEach(tr => {
+      tr.style.backgroundColor = null
+      tr.childNodes.forEach(th => th.style.backgroundColor = null)
+    })
+
+    if (e.target.cellIndex === 0) {
+      e.target.parentNode.style.backgroundColor = "#e6efff";
+    } else {
+      e.target.parentNode.style.backgroundColor = ""
+      e.target.style.backgroundColor = "#e6efff";
+    }
+    // console.log("value", initArray[tungdo][hoanhdo]);
+  };
+
+  const handleChangeCell = (e) => {
+    console.log(e.target.value);
   };
 
   return (
@@ -45,8 +74,8 @@ function App() {
         placeholder="column"
         onChange={(e) => setColNum(parseInt(e.target.value))}
       />
-      <button onClick={() => handleClick()}>Create</button>
-      <div className="scroll-x-able">
+      <button onClick={() => handleClickCreate()}>Create</button>
+      <div className="scroll">
         <table>
           <tbody>
             {initArray &&
@@ -54,7 +83,9 @@ function App() {
                 <>
                   <tr>
                     {i.map((j) => (
-                      <th>{j}</th>
+                      <th onClick={handleClickCell}>
+                        {j}
+                      </th>
                     ))}
                   </tr>
                 </>
@@ -65,5 +96,10 @@ function App() {
     </div>
   );
 }
+// const App = () => (
+//   <div style={{ width: 'max-content' }}>
+//     <Table x={50} y={10} />
+//   </div>
+// )
 
 export default App;
