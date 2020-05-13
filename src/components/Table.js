d8c9d36a023254e14aab69151b84d8d5a9bd35a0
Thai Nguyen <thainq00@gmail.com>
2020-05-13 16:59:17 +0700

    fix resize column

--- src/components/Table.js
+++ src/components/Table.js
@@ -280,8 +280,54 @@ function Table() {
   // const resizeColumnOut = (e) => {
   //   const th = e.target.parentNode;
   //   console.log(2);
-  //   th.style.width = diffX.toString() + "px";
+  //   th.style.width = diffX + "px";
   // };
+  useEffect(() => {
+    var thElm;
+    var startOffset;
+
+    Array.prototype.forEach.call(
+      document.querySelectorAll("table th"),
+      function (th) {
+        th.style.position = "relative";
+
+        var grip = document.createElement("div");
+        grip.innerHTML = "&nbsp;";
+        grip.style.top = 0;
+        grip.style.right = 0;
+        grip.style.bottom = 0;
+        grip.style.width = "5px";
+        grip.style.position = "absolute";
+        grip.style.cursor = "col-resize";
+        grip.addEventListener("mousedown", function (e) {
+          thElm = th;
+          startOffset = th.offsetWidth - e.pageX;
+        });
+
+        th.appendChild(grip);
+      }
+    );
+
+    document.addEventListener("mousemove", function (e) {
+      if (thElm) {
+        thElm.style.width = startOffset + e.pageX + "px";
+      }
+    });
+
+    document.addEventListener("mouseup", function () {
+      thElm = undefined;
+    });
+    return () => {
+      document.removeEventListener("mousemove", function (e) {
+        if (thElm) {
+          thElm.style.width = startOffset + e.pageX + "px";
+        }
+      });
+      document.removeEventListener("mouseup", function () {
+        thElm = undefined;
+      });
+    };
+  });
 
   return (
     <div style={{ position: "relative" }}>
@@ -357,13 +403,13 @@ function Table() {
                       onClick={handleClickCell}
                     >
                       <input onChange={handleChangeCell} value={j} />
-                      <div
+                      {/* <div
                         className="resizeColumn"
                         // onMouseDown={resizeColumn}
                         // onMouseUp={resizeColumnOut}
                       >
                         &nbsp;
-                      </div>
+                      </div> */}
                     </th>
                   ))}
                 </tr>
