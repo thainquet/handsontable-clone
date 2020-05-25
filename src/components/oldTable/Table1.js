3704a491534fe4fc785b8908720d4e8beb5b25b0
Thai Nguyen <thainq00@gmail.com>
2020-05-25 14:34:10 +0700

    add selected cell

--- src/components/oldTable/Table1.js
+++ src/components/oldTable/Table1.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Table.css";
+import $ from "jquery";
+window.$ = $;
 
 const makeid = (length) => {
   let result = "";
@@ -33,6 +35,76 @@ function Table() {
 
   let count = -1; // index for first row and column
   let keycount = 0; // key in loop render
+
+  useEffect(() => {
+    var table = $("#tbl");
+
+    var isMouseDown = false;
+    var startRowIndex = null;
+    var startCellIndex = null;
+
+    function selectTo(cell) {
+      var row = cell.parent();
+      var cellIndex = cell.index();
+      var rowIndex = row.index();
+
+      var rowStart, rowEnd, cellStart, cellEnd;
+
+      if (rowIndex < startRowIndex) {
+        rowStart = rowIndex;
+        rowEnd = startRowIndex;
+      } else {
+        rowStart = startRowIndex;
+        rowEnd = rowIndex;
+      }
+
+      if (cellIndex < startCellIndex) {
+        cellStart = cellIndex;
+        cellEnd = startCellIndex;
+      } else {
+        cellStart = startCellIndex;
+        cellEnd = cellIndex;
+      }
+
+      for (var i = rowStart; i <= rowEnd; i++) {
+        var rowCells = table.find("tr").eq(i).find("th");
+        for (var j = cellStart; j <= cellEnd; j++) {
+          rowCells.eq(j).addClass("selected");
+        }
+      }
+    }
+
+    table
+      .find("th")
+      .mousedown(function (e) {
+        isMouseDown = true;
+        var cell = $(this);
+
+        table.find(".selected").removeClass("selected"); // deselect everything
+
+        if (e.shiftKey) {
+          selectTo(cell);
+        } else {
+          cell.addClass("selected");
+          startCellIndex = cell.index();
+          startRowIndex = cell.parent().index();
+        }
+
+        return false; // prevent text selection
+      })
+      .mouseover(function () {
+        if (!isMouseDown) return;
+        table.find(".selected").removeClass("selected");
+        selectTo($(this));
+      })
+      .bind("selectstart", function () {
+        return false;
+      });
+
+    $(document).mouseup(function () {
+      isMouseDown = false;
+    });
+  });
 
   useEffect(() => {
     let arr = JSON.parse(JSON.stringify(initArray)); // deep clone
@@ -543,7 +615,7 @@ function Table() {
       <br />
       <br />
       <div id="table-data" className="scrollable">
-        <table>
+        <table id="tbl">
           <tbody id="tbody">
             {initArray &&
               initArray.map((i, rowIndex) =>
