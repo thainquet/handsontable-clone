08501e0b0b3d9c280854ca5051bed812beff1826
Thai Nguyen <thainq00@gmail.com>
2020-05-25 14:34:27 +0700

    fix in new table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Table.css";
 
 const Cell = (props) => {
@@ -7,24 +7,91 @@ const Cell = (props) => {
   const handleCellChange = (e) => {
     setLocalData(e.target.value);
   };
+
+  const clearAllCellBorder = () => {
+    Array.prototype.forEach.call(document.querySelectorAll("td"), function (e) {
+      e.classList.remove("cellSelected");
+    });
+  };
+  const handleClickCell = (event) => {
+    clearAllCellBorder();
+    event.target.parentNode.classList.add("cellSelected");
+  };
   return (
     <td>
-      <input value={localData} onChange={handleCellChange} />
+      <input
+        value={localData}
+        onChange={handleCellChange}
+        onClick={handleClickCell}
+      />
     </td>
   );
 };
 
+const clearAllBorder = (className) => {
+  Array.prototype.forEach.call(document.querySelectorAll("tr"), function (e) {
+    e.classList.remove(className);
+  });
+};
+
 const Row = (props) => {
+  useEffect(() => {
+    let thElm;
+    let startOffset;
+
+    Array.prototype.forEach.call(
+      document.querySelectorAll("table td#row"),
+      function (td) {
+        td.style.position = "relative";
+
+        let grip = document.createElement("div");
+        grip.innerHTML = "&nbsp;";
+        grip.style.bottom = 0;
+        grip.style.left = 0;
+        grip.style.right = 0;
+        grip.style.height = "2px";
+        grip.style.position = "absolute";
+        grip.style.cursor = "row-resize";
+        grip.addEventListener("mousedown", function (e) {
+          thElm = td;
+          startOffset = td.offsetHeight - e.pageY;
+        });
+
+        td.appendChild(grip);
+      }
+    );
+
+    document.addEventListener("mousemove", function (e) {
+      if (thElm) {
+        thElm.style.height = startOffset + e.pageY + "px";
+      }
+    });
+
+    document.addEventListener("mouseup", function () {
+      thElm = undefined;
+    });
+    return () => {
+      document.removeEventListener("mousemove", function (e) {
+        if (thElm) {
+          thElm.style.height = startOffset + e.pageY + "px";
+        }
+      });
+      document.removeEventListener("mouseup", function () {
+        thElm = undefined;
+      });
+    };
+  }, []);
   const { rowData } = props;
 
   const handleClickRow = (e) => {
     e.preventDefault();
     let thisTR = e.target.parentNode.parentNode;
-    thisTR.style.border = "2px solid #4b89ff";
+    clearAllBorder("rowSelected");
+    if (thisTR.tagName === "TR") thisTR.classList.add("rowSelected");
   };
   return (
     <tr>
-      <td onClick={handleClickRow}>
+      <td onClick={handleClickRow} id="row">
         <input disabled={true} />
       </td>
       {rowData &&
@@ -36,6 +103,48 @@ const Row = (props) => {
 };
 
 const Table = (props) => {
+  useEffect(() => {
+    let thElm;
+    let startOffset;
+
+    Array.prototype.forEach.call(
+      document.querySelectorAll("table th"),
+      function (th) {
+        th.style.position = "relative";
+
+        let grip = document.createElement("div");
+        grip.innerHTML = "&nbsp;";
+        grip.classList.add("resizeColumn");
+        grip.addEventListener("mousedown", function (e) {
+          thElm = th;
+          startOffset = th.offsetWidth - e.pageX;
+        });
+
+        th.appendChild(grip);
+      }
+    );
+
+    document.addEventListener("mousemove", function (e) {
+      if (thElm) {
+        thElm.style.width = startOffset + e.pageX + "px";
+      }
+    });
+
+    document.addEventListener("mouseup", function () {
+      thElm = undefined;
+    });
+    return () => {
+      document.removeEventListener("mousemove", function (e) {
+        if (thElm) {
+          thElm.style.width = startOffset + e.pageX + "px";
+        }
+      });
+      document.removeEventListener("mouseup", function () {
+        thElm = undefined;
+      });
+    };
+  });
+
   const { tableData } = props;
   const [initArray, setInitArray] = useState([
     ["a", "b", "c", "d", "e", "f", "G"],
