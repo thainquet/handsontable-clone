9f9e8ccb438592c5f1fdccca6a31dd5b9db330d3
Thai Nguyen <thainq00@gmail.com>
2020-05-27 16:13:03 +0700

    split context menu

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Table.css";
 import dummyData from "./dummyData.js";
+import ContextMenu from "./ContextMenu";
 
 const clearAllTRBorder = () => {
   Array.prototype.forEach.call(document.querySelectorAll("tr"), function (e) {
@@ -482,12 +483,6 @@ const Table = (props) => {
   const [visible, setVisible] = useState(false);
   const [top, setTop] = useState(0);
   const [left, setLeft] = useState(0);
-  const menuStyle = {
-    position: "absolute",
-    top: `${top}px`,
-    left: `${left}px`,
-    zIndex: "100",
-  };
   useEffect(() => {
     const handleRightClick = (event) => {
       event.preventDefault();
@@ -551,86 +546,16 @@ const Table = (props) => {
   });
 
   useEffect(() => console.log(initArray), [initArray]);
-  const handleInsertRow = (event) => {
-    event.preventDefault();
-    const position = event.target.getAttribute("data-position");
-    const thisTH = document.elementFromPoint(left - 5, top - 5);
-    const indexOfRowAndArrayItem = thisTH.parentNode.parentNode.rowIndex;
-    let tempArr = [...initArray];
-    const lengthItem = tempArr[0].length;
-    const Item = new Array(lengthItem).fill("");
-    if (position === "below") {
-      tempArr.splice(indexOfRowAndArrayItem + 1, 0, Item);
-    }
-    if (position === "above") {
-      tempArr.splice(indexOfRowAndArrayItem, 0, Item);
-    }
-    setInitArray(tempArr);
-    // console.log(position, thisTH);
-  };
 
   return (
     <>
-      <div className={visible ? "menu" : "menu hiden"} style={menuStyle}>
-        <div
-          className="menu-line"
-          data-position="right"
-          // onClick={handleInsertColumn}
-        >
-          Insert column right
-        </div>
-        <div
-          className="menu-line borderBottom"
-          data-position="left"
-          // onClick={handleInsertColumn}
-        >
-          Insert column left
-        </div>
-        <div
-          className="menu-line"
-          // onClick={handleDeleteColumn}
-        >
-          Delete this column
-        </div>
-        <div
-          className="menu-line borderBottom"
-          // onClick={handleDeleteColumnContent}
-        >
-          Delete this column content
-        </div>
-        <div
-          className="menu-line borderBottom"
-          //  onClick={handleUndo}
-        >
-          Undo
-        </div>
-        <div
-          className="menu-line"
-          data-position="above"
-          onClick={handleInsertRow}
-        >
-          Insert row above
-        </div>
-        <div
-          className="menu-line borderBottom"
-          data-position="below"
-          onClick={handleInsertRow}
-        >
-          Insert row below
-        </div>
-        <div
-          className="menu-line"
-          // onClick={handleDeleteRow}
-        >
-          Delete this row
-        </div>
-        <div
-          className="menu-line"
-          // onClick={handleDeleteRowContent}
-        >
-          Delete this row content
-        </div>
-      </div>
+      <ContextMenu
+        top={top}
+        left={left}
+        initArray={initArray}
+        visible={visible}
+        setInitArray={setInitArray}
+      />
 
       <table id="tbl">
         <thead>
